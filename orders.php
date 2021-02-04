cb14344687c0c7c00ebf52af94c9200a88ca99b4
Maciej-R <macrysnik@gmail.com>
2021-02-04 01:56:56 +0100

    Bug fixes

--- orders.php
+++ orders.php
@@ -1,8 +1,8 @@
 <?php
 include "DBConnection.php";
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    session_start();
     if (!isset($_SESSION['id'])) {
         echo '<a href="/login.php">Login first</a>';
         http_response_code(401);
@@ -21,37 +21,78 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
             if ($res['available'] < $_POST[$id]) throw new RuntimeException();
             $total += intval($_POST[$id]) * intval($res['price']);
         }
-        echo 'Checked';
         // Update availability
         $stmt = $dbconn -> prepare('update shop.products set available = ((select available from shop.products where id = :id)-'.$_POST[$id].') where id = :id');
         foreach (array_keys($_POST) as $id) {
             $success = $stmt -> execute([':id' => $id]);
             if (!$success) throw new RuntimeException();
         }
-        echo 'Updated';
         // Create new order entry
         $stmt = $dbconn -> prepare('insert into shop.orders (client, total, "date") values (:uid, :total, current_date) returning id');
         $success = $stmt -> execute([':uid' => $_SESSION['id'], ':total' => $total]);
         $oid = $stmt -> fetch()['id'];
-        echo 'Order created';
         // All order parts inserted
         $stmt = $dbconn -> prepare('insert into shop.parts (pid, oid, price, amount) values (:pid, :oid, :price, :amount)');
         $price = $dbconn -> prepare('select price from shop.products where id = :id');
         foreach (array_keys($_POST) as $id) {
-            echo $id;
             $price -> execute([':id' => $id]);
             $p = $price -> fetch()['price'];
-            echo $p;
             $stmt -> execute([':pid' => $id, ':oid' => $oid, ':price' => $p, ':amount' => intval($_POST[$id])]);
         }
-        echo 'Done';
         $dbconn->commit();
-        http_send_status(200);
+        //http_send_status(200);
     }catch (Exception $e){
         echo 'Fail';
-        echo $e;
         $dbconn -> rollBack();
-        http_send_status(500);
+        http_response_code(500);
     }
-    readfile('show_oreder.php');
 }
+
+echo '<!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>BD G22</title>
+       <link href="css/bootstrap.min.css" rel="stylesheet">
+   </head>
+   <body>';
+$dbconn = Connection::getPDO();
+$stmt = $dbconn -> prepare('select id from orders where client = :id');
+$success = $stmt -> execute([':id' => $_SESSION['id']]);
+if ($success) {
+    $data = $stmt -> fetchALL(PDO::FETCH_ASSOC);
+    foreach ($data as $row) {
+        echo 'Order ' . $row['id'];
+        echo '<div class="table-responsive cart_info">
+                <table class="table table-condensed">
+				    <thead>
+				    	<tr class="cart_menu">
+				    		<td class="description" style="width: 50%">Product</td>
+				    		<td class="price" style="width: 30%">Price</td>
+				    		<td class="quantity" style="width: 20%">Amount</td>
+				    	</tr>
+				    </thead>
+				<tbody id="item_table">';
+        $stmt = $dbconn -> prepare('select pid, amount, price from shop.parts where oid = :oid');
+        $success = $stmt -> execute(['oid' => $row['id']]);
+        if ($success) {
+            $parts = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+            foreach ($parts as $p) {
+                echo '<tr>';
+                $stmt = $dbconn -> prepare('select name from products where id = :pid');
+                $stmt -> execute([':pid' => $p['pid']]);
+                $name = $stmt -> fetch(PDO::FETCH_ASSOC)['name'];
+                echo '<td>' . $name . '</td>';
+                echo '<td>' . $p['price'] . '</td>';
+                echo '<td>' . $p['amount'] . '</td>';
+                echo '</tr>';
+            }
+        }
+        echo '</tbody>
+			</table>
+			</div>
+		</body>';
+    }
+}
+http_response_code(200);
